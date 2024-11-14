f223fbfa80dde5a3efd1f1ca4186118fb578a9d6
Setareh <setasabi@gmail,com>
2024-11-14 20:49:39 +0330

    commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-#Descriptor for rang of character
+# Descriptor for rang of character
 class RangeCharacter:
-    def __init__(self,min=2,max=10):
-        self.max=max
-        self.min=min
+    def __init__(self, min=2, max=10):
+        self.max = max
+        self.min = min
+
     def __set_name__(self, owner, name):
         self._attribute_name = name
 
@@ -10,19 +11,18 @@ class RangeCharacter:
         return instance.__dict__[self._attribute_name]
 
     def __set__(self, instance, value):
-        if not isinstance(value,str) or len(value)>self.max or len(value)<self.min:
+        if not isinstance(value, str) or len(value) > self.max or len(value) < self.min:
             raise ValueError(f'Invalid value for {self._attribute_name}')
 
         instance.__dict__[self._attribute_name] = value
 
 
-
 class User:
-    username= RangeCharacter()
+    username = RangeCharacter()
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
 
-User("star3","111")
+User("star3", "111")
